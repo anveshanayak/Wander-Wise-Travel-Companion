874a87585a9ea202f813fa2ae249d61170c640f3
Anvesha Nayak <anveshalnayak@gmail.com>
2023-09-10 20:14:14 +0530

    Update and rename env.d.ts to additional.d.ts

---
+++ additional.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    NEXT_PUBLIC_BASE_URL: string;
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: string;
+    NEXT_PUBLIC_TRAVEL_ADVISOR_KEY: string;
+    NEXT_PUBLIC_WEATHER_API_KEY: string;
+  }
+}
